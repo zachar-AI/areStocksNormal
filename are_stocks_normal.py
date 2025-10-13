4ec3f255ac060f815104950a703d6c3510bb479b
zachar-AI <zachskihill@gmail.com>
2025-10-12 21:19:23 -0400

    making things look nicer

--- are_stocks_normal.py
+++ are_stocks_normal.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 from datetime import date
 
 # TITLE
-st.title('Do Stock Returns Follow a Normal Distribution?')
+st.title('Do Stock Returns Follow a Normal Distribution (AKA Bell Curve🔔)?')
+st.markdown('#')
 
 # USER INPUT
 col1, col2 = st.columns(2)
 ui_ticker = col1.selectbox('Pick a stock ticker', ['^GSPC', 'AAPL', 'GOOG', 'TSLA', 'BTC-USD', 'ETH-USD'])
 
 intervals = ['1d', '1wk', '1mo', '3mo']
-ui_interval = col2.selectbox('Pick a time interval', intervals)
+ui_interval = col2.selectbox('Pick a time interval', intervals, index=1)
 
 col1, col2, col3, col4 = st.columns(4)
 curr_year = date.today().year
@@ -49,14 +50,42 @@ if valid:
     n = len(data)
     var = np.power(data['return'] - mu, 2).sum()/(n-1)
     std = np.sqrt(var)
+
     skew = np.power((data['return'] - mu)/std, 3).sum()*n/((n-1)*(n-2))
+    if skew < -0.5:
+        skew_msg = ('the distribution has a long left tail because people were panic selling! '
+        'This indicates the returns do not follow a normal distribution')
+    elif skew < 0.5:
+        skew_msg = ('the distribution is relatively symetric '
+        'This indicates the returns follow a normal distribution')
+    else:
+        skew_msg = ('the distribution has a long right tail because people are euphoric buying! '
+        'This indicates the returns do not follow a normal distribution')
+
     ex_kurt = np.power((data['return'] - mu)/std, 4).sum()*n*(n+1)/((n-1)*(n-2)*(n-3)) - 3*(n-1)**2 / ((n-2)*(n-3))
+    if ex_kurt < -0.5:
+        kurt_msg = ('the distribution has light 🐥 tails because this stock is boring and doesnt move much. '
+        'This indicates the returns do not follow a normal distribution')
+    elif ex_kurt < 0.5:
+        kurt_msg = ('the distribution has "normal" weight in the tails '
+        'This indicates the returns follow a normal distribution')
+    else:
+        kurt_msg = ('the distribution has heavy 🦍 tails because the stock has extreme price moves up and down. '
+        'This is likely due to earnings reports and investors reacting to breaking news! '
+        'This indicates the returns do not follow a normal distribution')
 
     # DISPLAY RESULTS
+    st.markdown('#')
     st.subheader('Do the returns look like a bell curve?')
-    st.write('Note that a bell curve should be symetric (skew = 0) without too much weight in the tails (kurtosis=0)')
-    st.write(f'Skew = {skew:.2f}')
-    st.write(f'Excess Kurtosis = {ex_kurt:.2f}')
+    st.text('Note that a bell curve should be symetric meaning it should be a mirror image of itself.\n'
+            'We can measure symetry with skewness which is 0 for a symetric distribution.\n\n'
+            f'The skew for the observed data is {skew:.2f} which means {skew_msg}')
+    st.text('')
+    st.text('Note that a bell curve should have a medium amount of weight in its tails.\n'
+            'This means that stocks have extreme moves ocassionally but not all the time\n'
+            'We can measure tail weight with excess kurtosis which is 0 for a normal distribution.\n\n'
+            f'The excess kurtosis for the observed data is {ex_kurt:.2f} which means {kurt_msg}')
+    st.markdown('#')
     fig, ax = plt.subplots()
     ax.hist(data['return'])
     ax.set_title(ui_ticker + " " + ui_interval + " returns")
@@ -64,7 +93,7 @@ if valid:
     ax.set_ylabel("frequency")
     st.pyplot(fig)
 
-
+    st.markdown('#')
     st.write('Data Summary')
     data_summary = pd.DataFrame({
         'description': ['number of data points', 
